a318fd72de89f94493e7c2b0eb3ee1634ba0b48f
Peinthor Rene <peinthor@gmail.com>
2013-03-02 19:21:20 +0100

    add different dataprovider (http, filesystem)

--- honstats.py
+++ honstats.py
@@ -8,6 +8,26 @@ import configparser
 
 """honstats console statistics program for Heroes of Newerth
 """
+dp = None
+
+class DataProvider(object): pass
+
+class HttpDataProvider(DataProvider):
+    def __init__(self, url = 'http://api.heroesofnewerth.com/', token=None):
+        self.url = url
+        self.token = token
+
+    def fetch(self, path):
+        resp = urllib.request.urlopen(self.url + path + "/?token=" + self.token)
+        return json.loads(resp.read().decode('utf-8'))
+
+class FSDataProvider(DataProvider):
+    def __init__(self, url = './sampledata'):
+        self.url = os.path.abspath(url)
+
+    def fetch(self, path):
+        with open(os.path.join(self.url, path)) as fd:
+            return json.load(fd)
 
 def nickoraccountid(id):
     try:
@@ -15,54 +35,70 @@ def nickoraccountid(id):
         return '/accountid/' + id
     except ValueError:
         return '/nickname/' + id
-        
-def fetchdata(url):
-    resp = urllib.request.urlopen(url)
-    return json.loads(resp.read().decode('utf-8'))
 
 def playercommand(args):
+    headerformat = "{nick:<10s} {mmr:<5s} {k:<6s} {d:<6s} {a:<6s} {wg:<3s} {cd:<5s} {kdr:<5s} {gp:<4s} {wp:<2s}"
+    playerformat = "{nick:<10s} {rank:<5d} {k:<6d}/{d:<6d}/{a:<6d} {wg:3.1f} {cd:4.1f} {kdr:5.2f}  {pg:<4d} {wp:2.0f}"
+
+    print(headerformat.format(nick="Nick",
+                              mmr="MMR",
+                              k="K",
+                              d="D",
+                              a="A",
+                              wg="W/G",
+                              cd="CD",
+                              kdr="KDR",
+                              gp="GP",
+                              wp="W%"))
+
+    statsmapping = { 'ranked': 'rnk', 'public': 'acc', 'casual': 'cs'}
+    prefix = statsmapping[args.statstype]
     for id in args.id:
-        url = args.host + '/player_statistics/' + args.statstype + nickoraccountid(id) + '/?token=' + args.token
-        print(url)
-        data = fetchdata(url)
-        print(json.dumps(data))
-        headerformat = "{nick:<10s} {mmr:<5s} {k:<4s} {d:<4s} {a:<4s} {wg:<3s} {cd:<4s} {kdr:<4s} {mgp:<4s} {wp:<2s}"
-        playerformat = "{nick:<10s} {rank:<5d} {k:<4d}/{d:<4d}/{a:<4d}"
-        print(headerformat.format(nick="Nick", mmr="MMR", k="K", d="D", a="A", wg="W/G", cd="CD", kdr="KDR", mgp="MGP", wp="W%"))
-        print(playerformat.format(nick=id, rank=int(float(data['rnk_amm_team_rating'])), k=int(data['rnk_herokills']), d=int(data['rnk_deaths']), a=int(data['rnk_heroassists'])))
-    
+        #print(url)
+        data = dp.fetch('player_statistics/' + args.statstype + nickoraccountid(id))
+        #print(json.dumps(data))
+        print(playerformat.format(nick=id,
+                                  rank=int(float(data[prefix + '_amm_team_rating'])),
+                                  k=int(data[prefix + '_herokills']),
+                                  d=int(data[prefix + '_deaths']),
+                                  a=int(data[prefix + '_heroassists']),
+                                  wg=int(data[prefix + '_wards'])/int(data[prefix + '_games_played']),
+                                  cd=int(data[prefix + '_denies'])/int(data[prefix + '_games_played']),
+                                  kdr=int(data[prefix + '_herokills'])/int(data[prefix + '_deaths']),
+                                  pg=int(data[prefix + '_games_played']),
+                                  wp=int(data[prefix + '_wins'])/int(data[prefix + '_games_played'])*100))
+
 def matchescommand(args):
     for id in args.id:
-        url = args.host + '/match_history/' + args.statstype + nickoraccountid(id) + '/?token=' + args.token
-        print(url)
-        data = fetchdata(url)
+        path = 'match_history/' + args.statstype + nickoraccountid(id)
+        data = dp.fetch(path)
         print(json.dumps(data))
-        
+
 def matchcommand(args):
     print(args)
 
 def main():
     parser = argparse.ArgumentParser(description='honstats fetches and displays Heroes of Newerth statistics')
-    #parser.add_argument('--host', default='http://api.heroesofnewerth.com', help='statistic host provider')
-    parser.add_argument('--host', default='http://localhost:1234', help='statistic host provider')
+    #parser.add_argument('--host', default='http://api.heroesofnewerth.com/', help='statistic host provider')
+    parser.add_argument('--host', default='http://localhost:1234/', help='statistic host provider')
     parser.add_argument('-t', '--token', help="hon statistics token")
     parser.add_argument('-s', '--statstype', choices=['ranked', 'public', 'casual'], default='ranked', help='Statstype to show')
-    
+
     subparsers = parser.add_subparsers(help='honstats commands')
     playercmd = subparsers.add_parser('player', help='Show player stats')
     playercmd.set_defaults(func=playercommand)
     playercmd.add_argument('id', nargs='+', help='Player nickname or hon id')
-    
+
     matchescmd = subparsers.add_parser('matches', help='Show matches of a player(s)')
     matchescmd.set_defaults(func=matchescommand)
     matchescmd.add_argument('id', nargs='+', help='Player nickname or hon id')
-    
+
     matchescmd = subparsers.add_parser('match', help='Show stats for match(es)')
     matchescmd.set_defaults(func=matchcommand)
     matchescmd.add_argument('matchid', nargs='+', help='HoN match id')
-    
+
     args = parser.parse_args()
-    
+
     if not args.token:
         # read token from config file
         configpath = '/etc/honstats'
@@ -73,8 +109,11 @@ def main():
         cp = configparser.ConfigParser()
         cp.read(configpath)
         args.token = cp.get('auth', 'token')
-    
+
     if 'func' in args:
+        global dp
+        dp = FSDataProvider() #development url
+        #dp = HttpDataProvider(args.host, token=args.token)
         args.func(args)
     else:
         parser.print_help()
